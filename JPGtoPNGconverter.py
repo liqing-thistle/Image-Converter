5dac8628da2a194b32627ca2619dcac64e9d7dcf
Qing Li <qingli1121@gmail.com>
2023-02-11 19:43:52 -0600

    create JPGtoPNGconverter.py

---
+++ JPGtoPNGconverter.py
@@ -0,0 +1,30 @@
+import sys
+import os
+from PIL import Image
+
+# Use sys module to grab the first and second argument, which refers to folders containing .jpg files and .png files respectively.
+JPG_folder = sys.argv[1]
+PNG_folder = sys.argv[2]
+print(JPG_folder, PNG_folder)
+
+# Check if 'PNG_folder' exists. If not,  create it.
+isExist = os.path.exists('PNG_folder')
+if isExist is False:
+    print('PNG_folder folder does not exist')
+    os.makedirs('PNG_folder')
+    print('Directory PNG_folder created')
+else:
+    print('PNG_folder folder does exist')
+
+# Create a list including all .jpg files.
+jpg_images = os.listdir(JPG_folder)
+
+# Iterate over all .jpg files, convert them to .png files and store them in 'PNG_folder'.
+for image_name in jpg_images:
+    img = Image.open(f'{JPG_folder}{image_name}')
+    clean_name = os.path.splitext(image_name)[0]   # get the image name without the extension (.jpg)
+    img.save(f'{PNG_folder}{clean_name}.png')
+print('All done!')
+
+
+
